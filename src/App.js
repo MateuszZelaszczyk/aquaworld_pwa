d9c3a98a66e8ad96660fe056e99469ceb9ce5cfa
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2022-12-13 17:15:58 +0100

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage/HomePage.js";
-
+import LoginPage from "./LogInPage/LogIn.js"
+import RegisterPage from "./RegisterPage/RegisterPage.js";
+import RePassword from "./PasswordReset/RePassword.js";
+import NewPassword from "./PasswordReset/NewPassword.js";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<LoginPage/>}/>
+        <Route exact path="/register" element={<RegisterPage/>}/>
+        <Route exact path="/repassword" element={<RePassword/>}/>
+        <Route exact path="/newpassword" element={<NewPassword/>}/>
       </Routes>
     </BrowserRouter>
   );
