3a08f97fa9aa627b684ae61771c7350b0b8a7c3f
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-02-09 17:42:40 +0100

    added link for addaqu and myaqua

--- src/MainPage/Navi.js
+++ src/MainPage/Navi.js
@@ -1,41 +1,48 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import style from "./MainPage.module.css";
 import foto from "../Assets/IconFish.png";
 import UserMenu from "./UserMenu/UserMenu";
 import { NavLink } from "react-router-dom";
-const Navi =()=>{
-    const [visible,setVisible] = useState(false);
+const Navi = () => {
+  const [visible, setVisible] = useState(false);
 
-    function handleVisibleCl() {
-      setVisible(!visible);
-    }
-    return(
+  function handleVisibleCl() {
+    setVisible(!visible);
+  }
+  return (
     <nav className={style.MainNaviContainer}>
-    <div className={style.MainNaviMenu}>
-      <div className={style.LogoContainer}>
-        <p className={style.Logo}>AquaWorld</p>{" "}
-        <img className={style.Foto} src={foto} alt="user" />
-      </div>
+      <div className={style.MainNaviMenu}>
+        <NavLink to="/mainpage" className={style.LogoContainer}>
+          <p className={style.Logo}>AquaWorld</p>{" "}
+          <img className={style.Foto} src={foto} alt="user" />
+        </NavLink>
 
-      <input
-        className={style.Search}
-        type="text"
-        placeholder="Wyszukaj..."
-      />
-      <ul className={style.ElementsList}>
-        <li className={style.ListItem}>Moje akwaria</li>
-        <li className={style.ListItem}>+Dodaj post</li>
-        <li className={style.ListItem}>,<NavLink className={style.Link} to="/newaqua">+Dodaj akwarium</NavLink></li>
-        <li className={style.ListItem}>
-          <button className={style.ProfilContainer} onClick={() => setVisible(!visible)}>
-            Profil <img className={style.Foto} src={foto} alt="user" />
-          </button>
-        </li>
-      </ul>
-    </div>
-    {visible && <UserMenu visible={handleVisibleCl} actual={visible} />}
-    
-  </nav>)
-}
+        <input className={style.Search} type="text" placeholder="Wyszukaj..." />
+        <ul className={style.ElementsList}>
+          <li className={style.ListItem}>
+            <NavLink className={style.Link} to="/myaqua">
+              Moje akwaria
+            </NavLink>
+          </li>
+          <li className={style.ListItem}>+Dodaj post</li>
+          <li className={style.ListItem}>
+            <NavLink className={style.Link} to="/newaqua">
+              +Dodaj akwarium
+            </NavLink>
+          </li>
+          <li className={style.ListItem}>
+            <button
+              className={style.ProfilContainer}
+              onClick={() => setVisible(!visible)}
+            >
+              Profil <img className={style.Foto} src={foto} alt="user" />
+            </button>
+          </li>
+        </ul>
+      </div>
+      {visible && <UserMenu visible={handleVisibleCl} actual={visible} />}
+    </nav>
+  );
+};
 
 export default Navi;
