f60d6a218bd03ff7bf783343c8600f6ae22d5192
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-02-09 16:13:02 +0100

    create navi as other file

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import style from "./MainPage.module.css";
-import foto from "../Assets/IconFish.png";
-import UserMenu from "./UserMenu/UserMenu";
+import Navi from "./Navi";
 const MainPage = () => {
-  const [ visible, setVisble ] = useState(false);
-  function handleVisible() {
-    setVisble(!visible);
-  }
-
-  function handleVisibleCl() {
-    setVisble(false);
-  }
   return (
     <div>
-      <nav className={style.MainNaviContainer}>
-        <div className={style.MainNaviMenu}>
-          <div className={style.LogoContainer}>
-            <p className={style.Logo}>AquaWorld</p>{" "}
-            <img className={style.Foto} src={foto} alt="user" />
-          </div>
-
-          <input
-            className={style.Search}
-            type="text"
-            placeholder="Wyszukaj..."
-          />
-          <ul className={style.ElementsList}>
-            <li className={style.ListItem}>Moje akwaria</li>
-            <li className={style.ListItem}>+Dodaj post</li>
-            <li className={style.ListItem}>+Dodaj akwarium</li>
-            <li className={style.ListItem}>
-                <button className={style.ProfilContainer} onClick={handleVisible}>
-              Profil <img className={style.Foto} src={foto} alt="user" />
-              </button>
-            </li>
-          </ul>
-        </div>
-        {visible && <UserMenu visible={handleVisibleCl} />}
-      </nav>
-
+      <Navi/>
       <div className={style.MainContainer}></div>
     </div>
   );
