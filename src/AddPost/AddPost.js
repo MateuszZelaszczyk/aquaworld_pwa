620aeb3c065aea41d3f819745aec2de715ffb00b
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-02-14 14:57:39 +0100

    added posible many fotos

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import Navi from "../MainPage/Navi";
 import style from "./AddPost.module.css";
-import Avatar from "../Assets/Avatar.png";
 import { NavLink } from "react-router-dom";
 const NewPost = () => {
-  const [avatarShow, setAvatarShow] = useState(null);
-  const [avatarURL, setAvatarURL] = useState(Avatar);
-  const update = (e) => {
-    setAvatarURL(e.target.files[0]);
-    var file = e.target.files[0];
-    const objectURL = URL.createObjectURL(file);
-    setAvatarShow(objectURL);
+  const [fotoShow, setFotoShow] = useState([]);
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileChange = (index, event) => {
+    const newFiles = [...selectedFiles];
+    newFiles[index] = event.target.files[0];
+    setSelectedFiles(newFiles);
+
+    const fotos =[...fotoShow];
+    fotos[index]=URL.createObjectURL(event.target.files[0]);
+    setFotoShow(fotos);
+  };
+
+  const handleAddField = () => {
+    setSelectedFiles([...selectedFiles, null]);
+    setFotoShow([...fotoShow, null]);
   };
   return (
     <div>
@@ -37,18 +45,21 @@ const NewPost = () => {
             name="describe"
             placeholder="Napisz o co chodzi"
           />
-          <label className={style.NewPostLabel} htmlFor="foto">
-            Załącz zdjęcie
-          </label>
-          <div>
-            <input
-              className={style.NewPostFile}
-              onChange={(e) => update(e)}
-              name="foto"
-              type="file"
-            ></input>
-            <img className={style.Foto} src={avatarShow} alt=""></img>
-          </div>
+          {selectedFiles.map((file, index) => (
+            <div key={index} className={style.File}>
+              <label htmlFor="foto">Wybierz zdjęcie</label>
+              <input
+                className={style.NewPostFile}
+                onChange={(e) => handleFileChange(index, e)}
+                name="foto"
+                type="file"
+              ></input>
+              <img className={style.Foto} src={fotoShow[index]} alt=""></img>
+            </div>
+          ))}
+          <button className={style.AddFoto} type="button" onClick={handleAddField}>
+            Dodaj zdjęcie
+          </button>
           <div className={style.NewPostType}>
             <legend className={style.Legend} htmlFor="isPublic">
               Widoczność postu
@@ -81,14 +92,14 @@ const NewPost = () => {
               Utwórz
             </button>
             <button className={style.Back}>
-              <NavLink to="/mainpage" className={style.Back}>
+              <NavLink to="/profile/mainpage" className={style.BackLink}>
                 Anuluj
               </NavLink>
             </button>
           </div>
         </form>
       </div>
-      <div style={{height:"30px", margin:"30px"}}></div>
+      <div style={{ height: "30px", margin: "30px" }}></div>
     </div>
   );
 };
