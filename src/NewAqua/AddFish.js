7accdabd71a858a3afeaa8be9d02369188c73b40
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-03-22 15:38:52 +0100

    added send data to server

--- src/NewAqua/AddFish.js
+++ src/NewAqua/AddFish.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Navi from "../MainPage/Navi";
 import style from "./AddAqua.module.css";
 import Plus from "../Assets/plus.svg";
 import Delete from "../Assets/delete.svg";
+import axios from "axios";
+
+import InfoWindow from "../InfoWindow/InfoWindow";
 const AddFish = () => {
-  const [fish, setFish] = useState([{ fish: { name: "", quantity: 0 } }]);
+  const [color, setColor] = useState("red");
+  const [info, setInfo] = useState("");
+  const [header, setHeader] = useState("");
+  const token = localStorage.getItem("access");
+  const aquarium = useParams().id;
+  const [show, setShow] = useState(false);
+  const [fish, setFish] = useState([
+    { name: "", quantity: 0, aquarium: aquarium },
+  ]);
   const handleChange = (index, event) => {
     const values = [...fish];
-    values[index].fish[event.target.name] = event.target.value;
+    values[index][event.target.name] = event.target.value;
     setFish(values);
   };
   const handleAdd = () => {
-    setFish([...fish, { fish: { name: "", quantity: 0 } }]);
+    setFish([...fish, { name: "", quantity: 0, aquarium: aquarium }]);
   };
 
   const handleRemove = (index) => {
@@ -20,16 +32,77 @@ const AddFish = () => {
     values.splice(index, 1);
     setFish(values);
   };
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        setShow(false);
+      }, 2500);
+      return () => clearTimeout(timer);
+    }
+  }, [show]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8000/api/fish/",
+        { fish },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        setHeader("Udało się");
+        setInfo("Dane zostały zapisane");
+        setColor("green");
+        setShow(true);
+        setFish([]);
+      })
+      .catch((err) => {
+        if (err.response) {
+          if (
+            err.response.data[0]["non_field_errors"] &&
+            err.response.data[0]["non_field_errors"].includes(
+              "The fields aquarium, name must make a unique set."
+            )
+          ) {
+            setHeader("Błąd!");
+            setInfo("W tym akwarium są już ryby z tego gatunku");
+            setColor("red");
+            setShow(true);
+          } else {
+            setHeader("Błąd!");
+            setInfo(err.response.data + err.response.status);
+            setColor("red");
+            setShow(true);
+          }
+        }
+        else if(err.request){
+          setHeader("Błąd!");
+          setInfo(err.request);
+          setColor("red");
+          setShow(true);
+        }
+        else{
+          setHeader("Błąd!");
+          setInfo("Error" + err.message);
+          setColor("red");
+          setShow(true);
+        }
+      });
+  };
   return (
     <div>
-      <Navi show={'none'} />
+      <Navi show={"none"} />
       <div className={style.NewFishContainer}>
         <h1 className={style.NewAquaHeader}>Czas na nowe ryby</h1>
-        <form className={style.NewAquaForm}>
+        <form className={style.NewAquaForm} onSubmit={handleSubmit}>
           <div>
             <div className={style.LabelContainer}>
               <label htmlFor="name">Gatunek</label>
-              <label htmlFor="quantity" className={style.QuantityLabel}>Ilość</label>
+              <label htmlFor="quantity" className={style.QuantityLabel}>
+                Ilość
+              </label>
             </div>
 
             {fish.map((fish, index) => (
@@ -38,32 +111,47 @@ const AddFish = () => {
                   className={style.NameInput}
                   type="text"
                   name="name"
-                  value={fish.fish.name}
+                  value={fish.name}
                   onChange={(event) => handleChange(index, event)}
+                  required
                 />
                 <input
                   className={style.NumberInput}
                   type="number"
                   name="quantity"
-                  min={0}
-                  value={fish.fish.quantity}
+                  min={1}
+                  value={fish.quantity}
                   onChange={(event) => handleChange(index, event)}
+                  required
                 />
-                <button className={style.Deletebtn} type="button" onClick={() => handleRemove(index)}>
+                <button
+                  className={style.Deletebtn}
+                  type="button"
+                  onClick={() => handleRemove(index)}
+                >
                   <img src={Delete} alt="" />
                 </button>
               </div>
             ))}
-            <button className={style.PlusBtn} type="button" onClick={() => handleAdd()}>
+            <button
+              className={style.PlusBtn}
+              type="button"
+              onClick={() => handleAdd()}
+            >
               <img src={Plus} alt="" />
             </button>
           </div>
-          <button className={style.Save}>Zapisz</button>
+          <button type="submit" className={style.Save}>
+            Zapisz
+          </button>
         </form>
         <NavLink className={style.BackBtn} to="/profile/myaqua">
           Powrót
         </NavLink>
       </div>
+      {show && (
+        <InfoWindow message={info} show={show} header={header} color={color} />
+      )}
     </div>
   );
 };
--- src/NewAqua/AddPlants.js
+++ src/NewAqua/AddPlants.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Navi from "../MainPage/Navi";
 import style from "./AddAqua.module.css";
 import Plus from "../Assets/plus.svg";
 import Delete from "../Assets/delete.svg";
+import axios  from "axios";
+import { useParams } from "react-router-dom";
+import InfoWindow from "../InfoWindow/InfoWindow";
 const AddPlants = () => {
-  const [plants, setPLants] = useState([{ plant: { name: "", quantity: 0 } }]);
+  const [color, setColor] = useState("red");
+  const [info, setInfo] = useState("");
+  const [header, setHeader] = useState("");
+  const token = localStorage.getItem("access");
+  const aquarium = useParams().id;
+  const [show, setShow] = useState(false);
+  const [plants, setPLants] = useState([ { name: "", quantity: 0, aquarium: aquarium }]);
   const handleChange = (index, event, id) => {
     const values = [...plants];
-    values[index].plant[event.target.name] = event.target.value;
+    values[index][event.target.name] = event.target.value;
     setPLants(values);
   };
   const handleAdd = (id) => {
-      setPLants([...plants, { plant: { name: "", quantity: 0 } }]);
+      setPLants([...plants, { name: "", quantity: 0, aquarium: aquarium } ]);
   };
 
   const handleRemove = (index, id) => {
@@ -20,12 +29,70 @@ const AddPlants = () => {
       values.splice(index, 1);
       setPLants(values);
   };
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        setShow(false)
+      }, 2500);
+      return () => clearTimeout(timer);
+    }
+  }, [show]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8000/api/plants/",
+        { plants },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        setHeader("Udało się");
+        setInfo("Dane zostały zapisane")
+        setColor("green");
+        setShow(true);
+        setPLants([]);
+      })
+      .catch((err) => {
+        if (err.response) {
+          if (
+            err.response.data[0]["non_field_errors"] &&
+            err.response.data[0]["non_field_errors"].includes(
+              "The fields aquarium, name must make a unique set."
+            )
+          ) {
+            setHeader("Błąd!");
+            setInfo("W tym akwarium są już rośliny z tego gatunku");
+            setColor("red");
+            setShow(true);
+          } else {
+            setHeader("Błąd!");
+            setInfo(err.response.data + err.response.status);
+            setColor("red");
+            setShow(true);
+          }
+        }
+        else if(err.request){
+          setHeader("Błąd!");
+          setInfo(err.request);
+          setColor("red");
+          setShow(true);
+        }
+        else{
+          setHeader("Błąd!");
+          setInfo("Error" + err.message);
+          setColor("red");
+          setShow(true);
+        }
+      });
+  };
   return (
     <div>
       <Navi show={'none'} />
       <div className={style.NewPlantsContainer}>
         <h1 className={style.NewAquaHeader}>Czas na nowe rośliny</h1>
-        <form className={style.NewAquaForm}>
+        <form className={style.NewAquaForm} onSubmit={handleSubmit}>
           
           <div>
           <div className={style.LabelContainer}>
@@ -38,16 +105,18 @@ const AddPlants = () => {
                  className={style.NameInput}
                   type="text"
                   name="name"
-                  value={plant.plant.name}
+                  value={plant.name}
                   onChange={(event) => handleChange(index, event, "p")}
+                  required
                 />
                 <input
                  className={style.NumberInput}
                   type="number"
                   name="quantity"
                   min={0}
-                  value={plant.plant.quantity}
+                  value={plant.quantity}
                   onChange={(event) => handleChange(index, event, "p")}
+                  required
                 />
                 <button className={style.Deletebtn} type="button" onClick={() => handleRemove(index, "p")}>
                   <img src={Delete} alt="" />
@@ -65,6 +134,7 @@ const AddPlants = () => {
           Powrót
         </NavLink>
       </div>
+      {show &&<InfoWindow message={info} show={show} header= {header} color={color}/>}
     </div>
   );
 };
