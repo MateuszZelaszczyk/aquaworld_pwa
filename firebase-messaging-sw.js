f7ad185897856ecc9a3a58dbfbe70e2c7409517f
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-04-13 15:48:41 +0200

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+// eslint-disable-next-line no-undef
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+// eslint-disable-next-line no-undef
+firebase.initializeApp(firebaseConfig);
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+  };
+
+  // eslint-disable-next-line no-restricted-globals
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
